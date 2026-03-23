e4276243a5abf6872a393390ea9804739c56d184
brantshen <74973214+brantshen@users.noreply.github.com>
2026-03-23 10:54:31 +0300

    Made some changes

--- man.py
+++ man.py
@@ -4,6 +4,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, PasswordField, EmailField
 from wtforms.validators import InputRequired, Email, Length
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, logout_user, current_user, UserMixin, login_required
+
 
 app = Flask(__name__)
 
@@ -11,6 +13,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
 app.config['SECRET_KEY'] = '*hbiubc8uyew83uq87wg8q8wq8g8gwiug8'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+# Login
+
+login_manager = LoginManager(app)
+login_manager.login_view='login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_name = db.Column(db.String(20), unique = True, nullable=False)
+    password = db.Column(db.String(20), unique=True, nullable=False)
+
+class UserForm(FlaskForm):
+    user_name = StringField('User Name', validators=(InputRequired(), Length(min=6, max=20)))
+    password = PasswordField('Password', validators=(InputRequired(), Length(min=8, max=20)))
+
+class LoginForm(FlaskForm):
+    user_name = StringField('User Name', validators=(InputRequired(), Length(min=6, max=20)))
+    password = PasswordField('Password', validators=(InputRequired(), Length(min=8, max=20)))
+
 
 
 class Student(db.Model):
@@ -22,7 +43,7 @@ class Student(db.Model):
 class StudentRegisterForm(FlaskForm):
     student_name = StringField('Name: ', validators= [InputRequired(), Length(min=5, max=20)])   
     student_age = IntegerField('Age: ', validators=[InputRequired()]) 
-    email = StringField('Email', validators = [InputRequired(),Email(), Length(min=10, max=30)])
+    # email = StringField('Email', validators = [InputRequired(),Email(), Length(min=10, max=30)])
 
 class Person(db.Model):
     pers_id = db.Column(db.Integer, primary_key = True)
@@ -35,10 +56,48 @@ with app.app_context():
 
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Signup Form Page
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = UserForm()
+    return render_template('signup.html', form=form)
+
+
+# Login Form Page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+
+    return render_template('dash.html')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = StudentRegisterForm()
-    return render_template('index.html', form=form)
+    details = Student.query.all()
+
+    if form.validate_on_submit():
+        lname = form.student_name.data
+        myage = form.student_age.data
+        try:
+            person = Student(stud_name=lname, stud_age=myage)
+            db.session.add(person)
+            db.session.commit()
+            return redirect('/')
+        
+        except:
+            print('Data not inserted')
+
+    return render_template('index.html', form=form, details=details)
 
 
 
